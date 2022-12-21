b730b47a70ff7b112dcc3c060608bc4027db54b9
hem193 <hemanthcastel@gmail.com>
2022-12-21 22:46:37 +0530

    connected to DB

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const { connectDB } = require("./config/db");
 
 const PORT = 1338;
 
 //middlewares
 app.use(express.json());
 app.use(express.static("content"));
+app.use(express.urlencoded({ extends: false }));
 
 app.listen(PORT, () => {
   console.log("Server Running...app.");
+  connectDB();
 });
